23395b931499e48eb9f8811e2ea4076b14e642cf
AhmadAr010 <ah.arab50@gmail.com>
2024-04-03 00:49:56 +0200

    add belgium open questions

--- js/end.js
+++ js/end.js
@@ -66,7 +66,7 @@ const checkCurrentQuiz = () => {
         
         The Dutch are pioneers in various fields, from art and design to technology and sustainable living. Rembrandt and Van Gogh are celebrated Dutch artists whose works continue to inspire generations, while Dutch designers like Piet Mondrian and Gerrit Rietveld have left an indelible mark on the world of modern design. In terms of technology and innovation, the Netherlands leads the way with advancements in renewable energy, urban planning, and agriculture.`
         
-    } else if (queryString === "?quiz=be&score=") {
+    } else if (queryString === "?quiz=be") {
         // Show Netherlands, France, and Germany photos
         document.getElementById('quiz-link-1').innerHTML = `<a href="www.google.com">
         <img src="../media/Netherlands.jpg" alt="Quiz 1">
@@ -80,11 +80,19 @@ const checkCurrentQuiz = () => {
          <img src="../media/Germany.jpg" alt="Quiz 1">
          <p>Quiz 3</p>
         </a>`
-        document.getElementById('info').innerHTML =`Nestled between France, Germany, and the Netherlands, Belgium offers a unique blend of cultures, languages, and traditions. Its capital, Brussels, serves as the administrative center of the European Union, embodying Belgium's role as a crossroads of international diplomacy and cooperation.
+        document.getElementById('info').innerHTML =`In het land van België, waar chocolade de lucht vulde en stripfiguren avonturen beleefden, waren er tien vragen die de nieuwsgierige geesten uitdaagden.<br><br>
 
-        Beyond Brussels, Belgium's landscape is dotted with medieval towns, picturesque countryside, and architectural wonders. Cities like Bruges and Ghent transport visitors back in time with their cobblestone streets, historic churches, and enchanting canals. Antwerp, known as the diamond capital of the world, dazzles with its vibrant fashion scene and thriving port.
+        Het begon met de vraag naar de hoofdstad van België, die de reizigers naar Brussel leidde. Daar ontdekten ze de stad van chocolade, Brugge genaamd.<br><br>
         
-        Belgium's culinary heritage is equally impressive, with specialties like Belgian chocolates, waffles, and beers enjoyed around the globe. From indulging in pralines at renowned chocolatiers to savoring a Trappist beer brewed in centuries-old monasteries, Belgium offers a gastronomic journey like no other.`
+        Terwijl ze verder reisden, ontmoetten ze de iconische Kuifje, een beroemde Belgische stripfiguur. En ze bewonderden het Atomium in Brussel, een architectonisch wonder.<br><br>
+        
+        België was ook de thuisbasis van muzikale grootheden zoals Natalia, bekend als de 'Queen of Pop'. En ze lazen literaire meesterwerken zoals 'De Leeuw van Vlaanderen', geschreven door Hendrik Conscience.<br><br>
+        
+        Antwerpen stond bekend om zijn diamanten, terwijl surrealistische kunstenaars zoals René Magritte de wereld verbaasden met hun werken.<br><br>
+        
+        Gentenaars vierden hun bijnaam in Gent, terwijl Aalst bekend stond om zijn levendige carnavalsstoet.<br><br>
+        
+        En zo eindigde het verhaal van de tien vragen van België, een verhaal dat de rijke cultuur en geschiedenis van het land weerspiegelde.`
     } else if (queryString === "?quiz=du") {
         // Show Netherlands, Belgium, and farnce photos
         document.getElementById('quiz-link-1').innerHTML = `<a href="www.google.com">
